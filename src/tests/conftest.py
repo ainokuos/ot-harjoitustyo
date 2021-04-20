1d1d3f8afd9b8a3d65298e3ebb9204128a7475bb
Aino Kuosmanen <aino.kuosmanen@helsinki.fi>
2021-04-20 15:19:22 +0300

    testaus

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,4 +1,4 @@
 from initialize_database import initialize_database
 
 def pytest_configure():
-	initialize_database()
+        initialize_database()
--- src/tests/services/tracker_service_test.py
+++ src/tests/services/tracker_service_test.py
@@ -1,7 +1,7 @@
 import unittest
 from entities.note import Note
 from entities.user import User
-from services_tracker_service import tracker_service
+from services.tracker_service import tracker_service
 
-class TestTrackerService(unittest.TestCase):
+#class TestTrackerService(unittest.TestCase):
     
